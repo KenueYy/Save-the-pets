9980b3ce4f4e56c0e7eb332e1661532eccbf8931
KenueY <kenueyy@yandex.ru>
2022-11-30 16:58:31 +0700

    formatting

--- Assets/Scripts/LineDrawer/LinesDrawer.cs
+++ Assets/Scripts/LineDrawer/LinesDrawer.cs
@@ -4,12 +4,12 @@
 namespace LineDrawer {
 	public class LinesDrawer : MonoBehaviour {
 
-		[SerializeField]
-		private GameObject linePrefab;
+        [SerializeField]
+        private GameObject linePrefab;
         [SerializeField]
         private LayerMask cantDrawOverLayer;
 
-		[Space(30f)]
+        [Space(30f)]
         [SerializeField]
         private Gradient lineColor;
         [SerializeField]
@@ -17,21 +17,21 @@ namespace LineDrawer {
         [SerializeField]
         private float lineWidth;
 
-		private Line _currentLine;
+        private Line _currentLine;
 
         private void Update() {
-			if (Input.GetMouseButtonDown(0)) {
-				BeginDraw();
-			}
+            if (Input.GetMouseButtonDown(0)) {
+                BeginDraw();
+            }
 
-			if (_currentLine != null) {
-				Draw();
-			}
+            if (_currentLine != null) {
+                Draw();
+            }
 
-			if (Input.GetMouseButtonUp(0)) {
-				EndDraw();
-			}
-		}
+            if (Input.GetMouseButtonUp(0)) {
+                EndDraw();
+            }
+        }
 
         private void BeginDraw() {
             _currentLine = Instantiate(linePrefab, this.transform).GetComponent<Line>();
@@ -41,32 +41,32 @@ namespace LineDrawer {
             _currentLine.SetPointsMinDistance(linePointsMinDistance);
             _currentLine.SetLineWidth(lineWidth);
 
-		}
+        }
 
         private void Draw() {
-			Vector2 mousePosition = MousePosition.GetPointerPosition2D();
-			RaycastHit2D hit = Physics2D.CircleCast(mousePosition, lineWidth / 3f, Vector2.zero, 1f, cantDrawOverLayer);
+            Vector2 mousePosition = MousePosition.GetPointerPosition2D();
+            RaycastHit2D hit = Physics2D.CircleCast(mousePosition, lineWidth / 3f, Vector2.zero, 1f, cantDrawOverLayer);
 
-			if (hit) {
-				EndDraw();
-				return;
-			} 
+            if (hit) {
+                EndDraw();
+                return;
+            }
 
-			_currentLine.AddPoint(mousePosition);
-		}
+            _currentLine.AddPoint(mousePosition);
+        }
 
         private void EndDraw() {
 
-			if (_currentLine != null) {
+            if (_currentLine != null) {
 
-				if (_currentLine.pointsCount < 2) {
-					Destroy(_currentLine.gameObject);
-					return;
-				}
-				
+                if (_currentLine.pointsCount < 2) {
+                    Destroy(_currentLine.gameObject);
+                    return;
+                }
+
                 _currentLine.UsePhysics(true);
                 _currentLine = null;
-			}
-		}
-	}
+            }
+        }
+    }
 }
