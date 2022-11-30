be3394ce9a39cb006af97396caf751630984904a
KenueY <kenueyy@yandex.ru>
2022-11-30 16:59:32 +0700

    formating

--- Assets/Scripts/LineDrawer/Line.cs
+++ Assets/Scripts/LineDrawer/Line.cs
@@ -3,72 +3,72 @@ using System.Collections.Generic;
 
 
 namespace LineDrawer {
-	public class Line : MonoBehaviour {
+    public class Line : MonoBehaviour {
 
-		[SerializeField]
-		private LineRenderer lineRenderer;
+        [SerializeField]
+        private LineRenderer lineRenderer;
 
-		[SerializeField]
-		private EdgeCollider2D edgeCollider;
+        [SerializeField]
+        private EdgeCollider2D edgeCollider;
 
-		[SerializeField]
-		private Rigidbody2D rigidBody;
+        [SerializeField]
+        private Rigidbody2D rigidBody;
 
-		[SerializeField]
-		private float pointsMinDistance = 0.1f;
+        [SerializeField]
+        private float pointsMinDistance = 0.1f;
 
-		internal int pointsCount = 0;
+        internal int pointsCount = 0;
 
-		private List<Vector2> _points = new List<Vector2>();
-		private float _circleColliderRadius;
+        private List<Vector2> _points = new List<Vector2>();
+        private float _circleColliderRadius;
 
-		public void AddPoint(Vector2 newPoint) {
+        public void AddPoint(Vector2 newPoint) {
 
-			if (pointsCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < pointsMinDistance) {
-				return;
-			}
+            if (pointsCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < pointsMinDistance) {
+                return;
+            }
 
-			_points.Add(newPoint);
-			pointsCount++;
+            _points.Add(newPoint);
+            pointsCount++;
 
-			CircleCollider2D circleCollider = gameObject.AddComponent<CircleCollider2D>();
-			circleCollider.offset = newPoint;
-			circleCollider.radius = _circleColliderRadius;
+            CircleCollider2D circleCollider = gameObject.AddComponent<CircleCollider2D>();
+            circleCollider.offset = newPoint;
+            circleCollider.radius = _circleColliderRadius;
 
 
-			lineRenderer.positionCount = pointsCount;
-			lineRenderer.SetPosition(pointsCount - 1, newPoint);
+            lineRenderer.positionCount = pointsCount;
+            lineRenderer.SetPosition(pointsCount - 1, newPoint);
 
-			if (pointsCount > 1) {
-				edgeCollider.points = _points.ToArray();
-			}
-		}
+            if (pointsCount > 1) {
+                edgeCollider.points = _points.ToArray();
+            }
+        }
 
-		public void UsePhysics(bool usePhysics) {
-			// isKinematic = true;
-			rigidBody.isKinematic = !usePhysics;
-		}
+        public void UsePhysics(bool usePhysics) {
+            // isKinematic = true;
+            rigidBody.isKinematic = !usePhysics;
+        }
 
-		public void SetLineColor(Gradient colorGradient) {
-			lineRenderer.colorGradient = colorGradient;
-		}
+        public void SetLineColor(Gradient colorGradient) {
+            lineRenderer.colorGradient = colorGradient;
+        }
 
-		public void SetPointsMinDistance(float distance) {
-			pointsMinDistance = distance;
-		}
+        public void SetPointsMinDistance(float distance) {
+            pointsMinDistance = distance;
+        }
 
-		public void SetLineWidth(float width) {
-			lineRenderer.startWidth = width;
-			lineRenderer.endWidth = width;
+        public void SetLineWidth(float width) {
+            lineRenderer.startWidth = width;
+            lineRenderer.endWidth = width;
 
-			_circleColliderRadius = width / 2f;
+            _circleColliderRadius = width / 2f;
 
-			edgeCollider.edgeRadius = _circleColliderRadius;
-		}
+            edgeCollider.edgeRadius = _circleColliderRadius;
+        }
 
-		private Vector2 GetLastPoint() {
-			return (Vector2)lineRenderer.GetPosition(pointsCount - 1);
-		}
+        private Vector2 GetLastPoint() {
+            return (Vector2)lineRenderer.GetPosition(pointsCount - 1);
+        }
 
-	}
+    }
 }
