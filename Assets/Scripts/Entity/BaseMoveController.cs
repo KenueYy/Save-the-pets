ef281780ac0b53f7d74422adfeac32aae36fea72
KenueY <kenueyy@yandex.ru>
2022-12-02 15:19:29 +0700

    update controllers

--- Assets/Scripts/Entity/BaseMoveController.cs
+++ Assets/Scripts/Entity/BaseMoveController.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class BaseMoveController : MonoBehaviour
 {
-    [SerializeField] 
-    private float speed;
+    private float _speed;
 
-    [SerializeField]
     private GameObject _target;
+    private GameObject _moveObject;
 
     private void Update() {
-        transform.position = Vector2.MoveTowards(transform.position, _target.transform.position, speed * Time.deltaTime);
+        _moveObject.transform.position = Vector2.MoveTowards(transform.position, _target.transform.position, _speed * Time.deltaTime);
     }
 
-    public void Initialize(GameObject target) {
+    public void Initialize(GameObject moveObject,GameObject target, float speed) {
         _target = target;
+        _speed = speed;
+        _moveObject = moveObject;
     }
 
 }
