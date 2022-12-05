9174db74f04204f77568c9c0cd0455c159befe32
KenueY <kenueyy@yandex.ru>
2022-12-06 00:00:49 +0700

    ready

---
+++ Assets/Scripts/UI/WindowController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject window;
+
+    [SerializeField]
+    private Button closeButton;
+
+    [SerializeField]
+    private Button openButton;
+
+    private PanelClick _closePanel => GetComponentInChildren<PanelClick>(true);
+
+    #region MonoBehaviour
+
+    private void Awake() {
+        closeButton.onClick.AddListener(Close);
+        openButton.onClick.AddListener(Open);
+        _closePanel.onEmptyPanelClick += Close;
+    }
+    private void OnDisable() {
+        _closePanel.onEmptyPanelClick -= Close;
+    }
+    #endregion
+
+    private void Open() {
+        window.SetActive(true);
+    }
+
+    private void Close() { 
+        window.SetActive(false);
+    }
+}
