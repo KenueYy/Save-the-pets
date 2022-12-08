eabf816ebf9dcd1e40a19e6299466872f748ce70
KenueY <kenueyy@yandex.ru>
2022-12-08 21:29:08 +0700

    fix gamemanager subscribe

--- Assets/Scripts/UI/LevelList/LevelClickCallback.cs
+++ Assets/Scripts/UI/LevelList/LevelClickCallback.cs
@@ -1,15 +1,26 @@
+using GameManagers;
+using GameManagers.Interfaces;
 using System;
 using UnityEngine;
 
 namespace UI.LevelList {
     internal class LevelClickCallback : MonoBehaviour {
-        [SerializeField]
-        private LevelListUI levelList;
 
         public event Action<int> onLevelClicked;
 
-        private void Awake() {
+        [SerializeField]
+        private LevelListUI levelList;
+
+        private ILoadLevel _gameManager;
+
+        private void OnEnable() {
+            _gameManager = FindObjectOfType<GameManager>().GetComponent<ILoadLevel>();
             levelList.onItemClicked.AddListener(OnLevelItemClicked);
+            onLevelClicked += _gameManager.LoadLevel;
+        }
+
+        private void OnDisable() {
+            onLevelClicked -= _gameManager.LoadLevel;
         }
 
         private void OnLevelItemClicked(string levelIndex) {
